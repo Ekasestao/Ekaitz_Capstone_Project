0967c777bd09987e5b87848b771bc8a621b99e3f
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2023-08-29 19:34:07 +0200

    Capstone Project

--- DB/db.sql
+++ DB/db.sql
@@ -1,17 +1,18 @@
 CREATE TABLE users (
   users_id int NOT NULL AUTO_INCREMENT,
-  users_username varchar(30) NOT NULL,
-  users_email varchar(40) NOT NULL,
+  users_username varchar(30) NOT NULL UNIQUE,
+  users_email varchar(40) NOT NULL UNIQUE,
   users_password varchar(50) NOT NULL,
   users_name varchar(30) NOT NULL,
   users_lastname varchar(30) NOT NULL,
+  users_admin boolean NOT NULL DEFAULT 0,
   PRIMARY KEY (users_id),
   UNIQUE KEY users_id_UNIQUE (users_id)
 );
 
 CREATE TABLE products (
   products_id int NOT NULL AUTO_INCREMENT,
-  products_name varchar(30) NOT NULL,
+  products_name varchar(30) NOT NULL UNIQUE,
   products_description varchar(100) NOT NULL,
   products_price float NOT NULL,
   PRIMARY KEY (products_id),
