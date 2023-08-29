60e11c034c8dcf546b14f9ad610846f436406308
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2023-08-30 01:35:27 +0200

    Capstone Project

--- React Capstone Project/src/Components/App.jsx
+++ React Capstone Project/src/Components/App.jsx
@@ -1,28 +1,10 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 import Home from "./Home";
 
 class App extends Component {
   constructor() {
     super();
-
-    this.connectApi = this.connectApi.bind(this);
-  }
-
-  connectApi() {
-    axios
-      .get("http://ekasestao.pythonanywhere.com/users")
-      .then((response) => {
-        console.log("Connecting Api Response", response);
-      })
-      .catch((error) => {
-        console.log("Connecting Api Error", error);
-      });
-  }
-
-  componentDidMount() {
-    this.connectApi();
   }
 
   render() {
