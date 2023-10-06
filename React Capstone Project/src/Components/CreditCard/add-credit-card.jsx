048ecc2f715cfa4b5142bf1bf85627811f87a87e
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2023-10-06 23:49:40 +0200

    Capstone Project

--- React Capstone Project/src/Components/CreditCard/add-credit-card.jsx
+++ React Capstone Project/src/Components/CreditCard/add-credit-card.jsx
@@ -15,14 +15,13 @@ class AddCreditCard extends Component {
       cardMask: "#### #### #### ####",
       cardNumberTemp: "",
       isCardFlipped: false,
-      focusElementStyle: null,
+      focusedElementStyle: null,
       isInputFocused: false,
     };
 
     this.cardNumberRef = React.createRef();
     this.cardNameRef = React.createRef();
     this.cardDateRef = React.createRef();
-    this.cardCvvRef = React.createRef();
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.focusInput = this.focusInput.bind(this);
@@ -59,7 +58,7 @@ class AddCreditCard extends Component {
     const target = inputRef.current;
     if (target) {
       this.setState({
-        focusElementStyle: {
+        focusedElementStyle: {
           width: `${target.offsetWidth}px`,
           height: `${target.offsetHeight}px`,
           transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`,
@@ -71,7 +70,7 @@ class AddCreditCard extends Component {
   blurInput() {
     setTimeout(() => {
       if (!this.state.isInputFocused) {
-        this.setState({ focusElementStyle: null });
+        this.setState({ focusedElementStyle: null });
       }
     }, 300);
     this.setState({ isInputFocused: false });
@@ -89,7 +88,7 @@ class AddCreditCard extends Component {
   }
 
   componentDidMount() {
-    this.setState({ cardNumberTemp: this.cardMask }, () => {
+    this.setState({ cardNumberTemp: this.state.cardMask }, () => {
       const cardNumberInput = document.getElementById("cardNumber");
       if (cardNumberInput) {
         cardNumberInput.focus();
@@ -108,13 +107,20 @@ class AddCreditCard extends Component {
                   this.state.isCardFlipped ? "-active" : ""
                 }`}
               >
-                <div className="card-item__side -front">
+                <div
+                  className={`card-item__side -front ${
+                    this.state.isCardFlipped ? "-active" : ""
+                  }`}
+                >
                   <div
                     className={`card-item__focus ${
-                      this.state.focusElementStyle ? "-active" : ""
+                      this.state.isInputFocused ? "-active" : ""
                     }`}
-                    style={this.state.focusElementStyle}
-                    ref="focusElement"
+                    style={
+                      this.state.isInputFocused
+                        ? this.state.focusedElementStyle
+                        : null
+                    }
                   ></div>
                   <div className="card-item__cover">
                     <img
@@ -122,8 +128,137 @@ class AddCreditCard extends Component {
                       className="card-item__bg"
                     />
                   </div>
+
+                  <div className="card-item__wrapper">
+                    <div className="card-item__top">
+                      <img
+                        src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
+                        className="card-item__chip"
+                      />
+                      <div className="card-item__type">
+                        {this.getCardType() && (
+                          <img
+                            src={`https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/${this.getCardType()}.png`}
+                            key={this.getCardType()}
+                            alt=""
+                            className="card-item__typeImg"
+                          />
+                        )}
+                      </div>
+                    </div>
+                    <label
+                      htmlFor="cardNumber"
+                      className="card-item__number"
+                      ref={this.cardNumberRef}
+                    >
+                      {Array.from(this.state.cardMask).map((n, $index) => (
+                        <span key={$index}>
+                          {n === "#" ? (
+                            <div className="card-item__numberItem">
+                              {this.state.cardNumberTemp[$index] === "*"
+                                ? "*"
+                                : this.state.cardNumberTemp[$index]}
+                            </div>
+                          ) : (
+                            <div
+                              className={`card-item__numberItem ${
+                                n.trim() === "" ? "-active" : ""
+                              }`}
+                              key={$index}
+                            >
+                              {n}
+                            </div>
+                          )}
+                        </span>
+                      ))}
+                    </label>
+                    <div className="card-item__content">
+                      <label
+                        htmlFor="cardName"
+                        className="card-item__info"
+                        ref={this.cardNameRef}
+                      >
+                        <div className="card-item__holder">Titular</div>
+                        {this.state.cardName.length ? (
+                          <div className="card-item__name" key="1">
+                            <div className="card-item__nameItem">
+                              {this.state.cardName
+                                .replace(/\s\s+/g, " ")
+                                .split("")
+                                .map((n, $index) => (
+                                  <span key={$index + 1}>{n}</span>
+                                ))}
+                            </div>
+                          </div>
+                        ) : (
+                          <div className="card-item__name" key="2">
+                            Nombre Completo
+                          </div>
+                        )}
+                      </label>
+
+                      <div className="card-item__date" ref={this.cardDateRef}>
+                        <label
+                          htmlFor="cardYear"
+                          className="card-item__dateTitle"
+                        >
+                          Expira
+                        </label>
+                        <label
+                          htmlFor="cardYear"
+                          className="card-item__dateItem"
+                        >
+                          {this.state.cardYear ? (
+                            <span key={this.state.cardYear}>
+                              {String(this.state.cardYear).slice(2, 4)}
+                            </span>
+                          ) : (
+                            <span key="2">AA</span>
+                          )}
+                        </label>
+                        /
+                        <label
+                          htmlFor="cardMonth"
+                          className="card-item__dateItem"
+                        >
+                          {this.state.cardMonth ? (
+                            <span key={this.state.cardMonth}>
+                              {this.state.cardMonth}
+                            </span>
+                          ) : (
+                            <span key="2">MM</span>
+                          )}
+                        </label>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="card-item__side -back"></div>
+
+                <div className="card-item__side -back">
+                  <div className="card-item__cover">
+                    <img
+                      src={`https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/${this.state.currentCardBackground}.jpeg`}
+                      className="card-item__bg"
+                    />
+                  </div>
+                  <div className="card-item__band"></div>
+                  <div className="card-item__cvv">
+                    <div className="card-item__cvvTitle">CVV</div>
+                    <div className="card-item__cvvBand">
+                      {this.state.cardCvv.split("").map((_, index) => (
+                        <span key={index}>*</span>
+                      ))}
+                    </div>
+                    <div className="card-item__type">
+                      {this.getCardType() && (
+                        <img
+                          src={`https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/${this.getCardType()}.png`}
+                          className="card-item__typeImg"
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="card-form__inner">
@@ -136,13 +271,29 @@ class AddCreditCard extends Component {
                     type="text"
                     className="card-input__input"
                     id="cardNumber"
-                    ref={this.cardNumberRef}
+                    ref={this.cardNumber}
                     value={this.state.cardNumber}
                     onInput={(e) => {
                       const value = e.target.value.replace(/\D/g, "");
                       const formattedValue = value.replace(/(\d{4})/g, "$1 ");
-                      const newValue = formattedValue.trim().slice(0, 14);
-                      this.setState({ cardNumber: newValue });
+                      const newValue = formattedValue.slice(0, 19).trim();
+
+                      let cardNumberTemp = this.state.cardMask
+                        .split("")
+                        .map((char, index) => {
+                          if (char === "#" && formattedValue[index]) {
+                            if (index >= 5 && index <= 13) {
+                              return "*";
+                            }
+                            return formattedValue[index];
+                          }
+                          return char;
+                        });
+
+                      this.setState({
+                        cardNumber: newValue,
+                        cardNumberTemp: cardNumberTemp.join(""),
+                      });
                     }}
                     onFocus={() => this.focusInput(this.cardNumberRef)}
                     onBlur={this.blurInput}
@@ -157,12 +308,12 @@ class AddCreditCard extends Component {
                     type="text"
                     className="card-input__input"
                     id="cardName"
-                    ref={this.cardNameRef}
+                    ref={this.cardName}
                     value={this.state.cardName}
                     onInput={(e) => {
                       const newValue = e.target.value
                         .replace(/[^a-zA-Z ]/g, "")
-                        .slice(0, 30);
+                        .slice(0, 26);
                       this.setState({ cardName: newValue });
                     }}
                     onFocus={() => this.focusInput(this.cardNameRef)}
@@ -173,7 +324,7 @@ class AddCreditCard extends Component {
                 <div className="card-form__row">
                   <div className="card-form__col">
                     <div className="card-form__group">
-                      <label htmlFor="cardMonth" className="card-input__label">
+                      <label htmlFor="cardYear" className="card-input__label">
                         Válido Hasta
                       </label>
                       <select
@@ -250,7 +401,7 @@ class AddCreditCard extends Component {
                         ref={this.cardCvvRef}
                         value={this.state.cardCvv}
                         onFocus={() => {
-                          this.flipCard(true), this.focusInput(this.cardCvvRef);
+                          this.flipCard(true);
                         }}
                         onBlur={() => this.flipCard(false)}
                         autoComplete="off"
