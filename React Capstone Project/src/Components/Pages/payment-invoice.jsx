2abd7afddd1da7de7a7dc3da4049c3228f01a6f1
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2023-10-16 22:27:32 +0200

    Capstone Project

--- React Capstone Project/src/Components/Pages/payment-invoice.jsx
+++ React Capstone Project/src/Components/Pages/payment-invoice.jsx
@@ -10,68 +10,72 @@ function Invoice(props) {
     <div className="content-wrapper">
       <div className="payment-invoice">
         {Object.keys(invoiceObject).length !== 0 ? (
-          <div className="invoice-wrapper">
-            <div className="invoice">
-              <div className="invoice-top">
-                <div className="invoice-id-date">
-                  {invoiceObject.id ? (
-                    <div className="invoice-id">
-                      Nº Factura: {invoiceObject.id}
+          <div className="invoice-button-wrapper">
+            <div className="invoice-wrapper">
+              <div className="invoice-background">
+                <div className="invoice">
+                  <div className="invoice-top">
+                    <div className="invoice-id-date">
+                      {invoiceObject.id ? (
+                        <div className="invoice-id">
+                          Nº Factura: {invoiceObject.id}
+                        </div>
+                      ) : null}
+
+                      {invoiceObject.date ? (
+                        <div className="invoice-date">
+                          Fecha: {invoiceObject.date}
+                        </div>
+                      ) : null}
+                    </div>
+                  </div>
+
+                  {invoiceObject.name && invoiceObject.lastname ? (
+                    <div className="invoice-fullname">
+                      Nombre Completo: {fullName}
                     </div>
                   ) : null}
 
-                  {invoiceObject.date ? (
-                    <div className="invoice-date">
-                      Fecha: {invoiceObject.date}
-                    </div>
-                  ) : null}
-                </div>
-              </div>
-
-              {invoiceObject.name && invoiceObject.lastname ? (
-                <div className="invoice-fullname">
-                  Nombre Completo: {fullName}
-                </div>
-              ) : null}
-
-              <div className="invoice-bottom">
-                {props.invoice.products ? (
-                  <div className="invoice-products">
-                    Productos:
-                    {props.invoice.products.map((product) => (
-                      <div
-                        className="invoice-product"
-                        key={product.products_id}
-                      >
-                        <span>Nº Producto: {product.products_id}</span>
-                        <span>Nombre: {product.products_name}</span>
-                        <span>Precio: {product.products_price} €</span>
+                  <div className="invoice-bottom">
+                    {props.invoice.products ? (
+                      <div className="invoice-products">
+                        Productos:
+                        {props.invoice.products.map((product) => (
+                          <div
+                            className="invoice-product"
+                            key={product.products_id}
+                          >
+                            <span>Nº Producto: {product.products_id}</span>
+                            <span>Nombre: {product.products_name}</span>
+                            <span>Precio: {product.products_price} €</span>
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
-                ) : null}
+                    ) : null}
 
-                {invoiceObject.total ? (
-                  <div className="invoice-total">
-                    <span>
-                      Precio Total: {"  "}
-                      {parseFloat(invoiceObject.total).toFixed(2)} €
-                    </span>
+                    {invoiceObject.total ? (
+                      <div className="invoice-total">
+                        <span>
+                          Precio Total: {"  "}
+                          {parseFloat(invoiceObject.total).toFixed(2)} €
+                        </span>
+                      </div>
+                    ) : null}
+                    <div />
                   </div>
-                ) : null}
+                </div>
               </div>
-
-              <div className="invoice-button">
-                <button className="btn" onClick={() => props.navigate("/")}>
-                  Página principal
-                </button>
-              </div>
+            </div>
+            <div className="invoice-button">
+              <button className="btn" onClick={() => props.navigate("/")}>
+                Página principal
+              </button>
             </div>
           </div>
         ) : (
           <div className="no-invoice">
             <h2>No se ha encontrado la factura</h2>
-            <div className="invoice-button">
+            <div className="invoice-link">
               <Link to="/">Volver a la página principal</Link>
             </div>
           </div>
